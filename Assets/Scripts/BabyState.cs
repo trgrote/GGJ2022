3ed1dc2110a1a84038f60a501e7dc8055344b457
Taylor Grote <grotese@gmail.com>
2022-01-22 15:33:56 -0600

    Add dog and baby state

---
+++ Assets/Scripts/BabyState.cs
@@ -0,0 +1,6 @@
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "GGJ2022/Baby State")]
+public class BabyState : ScriptableObject
+{
+}
---
+++ Assets/Scripts/DogState.cs
@@ -0,0 +1,6 @@
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "GGJ2022/Dog State")]
+public class DogState : ScriptableObject
+{
+}
