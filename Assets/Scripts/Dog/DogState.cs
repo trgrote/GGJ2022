0f0858461933bb69a288669d9615e32a7f4668ee
Taylor Grote <grotese@gmail.com>
2022-01-30 14:54:41 -0600

    Add Dog states

--- Assets/Scripts/Dog/DogState.cs
+++ Assets/Scripts/Dog/DogState.cs
@@ -4,4 +4,7 @@ using UnityEngine;
 public class DogState : ScriptableObject
 {
     public Vector3 _currentMovement = Vector3.zero;
+
+    public float _badBadMeter = 0f;  // 0 -> 1f
+    public float _workMeter = 0f;   // 0 -> 1f
 }
