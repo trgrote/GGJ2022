8f2ad9f65a4876b234cd142e87677bc731d31d3d
Taylor Grote <grotese@gmail.com>
2022-02-19 13:02:29 -0600

    Add Baby Poop Event

--- Assets/Scripts/Baby/BabyPooper.cs
+++ Assets/Scripts/Baby/BabyPooper.cs
@@ -5,9 +5,11 @@ using UnityEngine;
 public class BabyPooper : MonoBehaviour
 {
     [SerializeField] GameObject _poopPrefab;
-    
+    [SerializeField] rho.Event _poopEvent;
+
     public void Poop()
     {
-        Instantiate(_poopPrefab, transform.position + new Vector3(0, 2f, 0f), transform.rotation);        
+        Instantiate(_poopPrefab, transform.position + new Vector3(0, 2f, 0f), transform.rotation);
+        _poopEvent.Raise();
     }
 }
